8927ac97ca01b4f6da12b2ad05cc45608715da55
Harishkumar93 <harishkumar.vanka@paradigmcreatives.com>
2015-03-24 13:07:49 +0530

    changes in install dependencies script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-yum groupinstall -y "Web Server" "MySQL Database" "PHP Support" >> /var/log/codedeploy-mysqld.log
+yum install httpd -y >> /var/log/codedeploy-httpd.log
+yum install mysql-server -y >> /var/log/codedeploy-mysql.log
 yum install -y php-mysql >> /var/log/codedeploy-mysqld.log

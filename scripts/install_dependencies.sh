ca6703320d1f81f51caa7605544a5f342ee209cd
Harishkumar93 <harishkumar.vanka@paradigmcreatives.com>
2015-03-24 13:16:33 +0530

    Changed the dependencies file

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum install httpd -y >> /var/log/codedeploy-httpd.log
 yum install mysql-server -y >> /var/log/codedeploy-mysql.log
-yum install -y php-mysql >> /var/log/codedeploy-mysqld.log
+yum --enablerepo=remi install php-common php-cli php-mysql php-devel >> /var/log/codedeploy-php.log

47a25053a605577da3d716c5702ab38364f122c3
Harishkumar93 <harishkumar.vanka@paradigmcreatives.com>
2015-03-24 13:28:28 +0530

    Install php

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum install httpd -y >> /var/log/codedeploy-httpd.log
 yum install mysql-server -y >> /var/log/codedeploy-mysql.log
-yum --enablerepo=remi install php-common php-cli php-mysql php-devel >> /var/log/codedeploy-php.log
+yum install php-common php-cli php-mysql php-devel -y >> /var/log/codedeploy-php.log
